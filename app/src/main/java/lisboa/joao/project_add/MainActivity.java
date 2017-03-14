41e42505cf39bd2344ea7fdf5c4fe590596d52d5
João Lisboa <joaolisboa@tuta.io>
2017-03-14 14:30:06 +0000

    Initial commit

---
+++ app/src/main/java/lisboa/joao/project_add/MainActivity.java
@@ -0,0 +1,13 @@
+package lisboa.joao.project_add;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
