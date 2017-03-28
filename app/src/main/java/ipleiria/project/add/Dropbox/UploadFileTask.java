31b503469afddbf13012eb1a36f6e84d8a3a200d
João Lisboa <joaolisboa@tuta.io>
2017-03-29 00:56:28 +0100

    UploadFileTask for Dropbox, from core-sdk example

--- app/src/main/java/ipleiria/project/add/Dropbox/UploadFileTask.java
+++ app/src/main/java/ipleiria/project/add/Dropbox/UploadFileTask.java
@@ -9,6 +9,7 @@ import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.FileMetadata;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -28,6 +29,7 @@ public class UploadFileTask extends AsyncTask<String, Void, FileMetadata> {
 
     public interface Callback {
         void onUploadComplete(FileMetadata result);
+
         void onError(Exception e);
     }
 
@@ -51,32 +53,12 @@ public class UploadFileTask extends AsyncTask<String, Void, FileMetadata> {
 
     @Override
     protected FileMetadata doInBackground(String... params) {
-        String localUri = params[0];
-        File localFile = UriHelper.getFileForUri(mContext, Uri.parse(localUri));
+        Uri localUri = Uri.parse(params[0]);
 
-        try {
-            //return mDbxClient.files().uploadBuilder(remoteFolderPath + "/" + remoteFileName)
-            //      .withMode(WriteMode.OVERWRITE)
-            //    .uploadAndFinish(inputStream);
-            return mDbxClient.files().upload("/test/hllo.txt").uploadAndFinish(new ByteArrayInputStream("hello".getBytes()));
+        try (InputStream is = mContext.getContentResolver().openInputStream(localUri)) {
+            return mDbxClient.files().upload(UriHelper.getFileName(mContext, localUri)).uploadAndFinish(is);
         } catch (DbxException | IOException e) {
             mException = e;
-        }
-
-        if (localFile != null) {
-            String remoteFolderPath = params[1];
-
-            // Note - this is not ensuring the name is a valid dropbox file name
-            String remoteFileName = localFile.getName();
-
-            try (InputStream inputStream = new FileInputStream(localFile)) {
-                //return mDbxClient.files().uploadBuilder(remoteFolderPath + "/" + remoteFileName)
-                  //      .withMode(WriteMode.OVERWRITE)
-                    //    .uploadAndFinish(inputStream);
-                return mDbxClient.files().upload("/test/hllo.txt").uploadAndFinish(new ByteArrayInputStream("hello".getBytes()));
-            } catch (DbxException | IOException e) {
-                mException = e;
-            }
         }
 
         return null;
