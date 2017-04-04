08636cfcb62aaefd5ff0826027ceda2c00fcb046
brunojoo <bruno.fonesca10@gmail.com>
2017-04-04 11:59:18 +0100

    ArrayAdapter to list items of dir

--- app/src/main/java/ipleiria/project/add/ListActivity.java
+++ app/src/main/java/ipleiria/project/add/ListActivity.java
@@ -2,26 +2,44 @@ package ipleiria.project.add;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
 
+    ArrayAdapter<String> adapter;
+
+    private LinkedList<String> list;
+    private ListView l ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_list);
+        l =(ListView) findViewById(R.id.listView);
+        list = new LinkedList<>();
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
+        l.setAdapter(adapter);
 
         File dir = getFilesDir();
-        ListView listView = (ListView)findViewById(R.id.listView);
+
+        list.add("qweqeq");
 
         for(File f: dir.listFiles()){
-            TextView tview = new TextView(this);
-            tview.setText(f.getName());
-            listView.addHeaderView(tview);
+            //TextView tview = new TextView(this);
+            //tview.setText(f.getName());
+            // listView.addHeaderView(tview);
+            list.add(f.getName());
+
         }
 
     }
+
+
 }
